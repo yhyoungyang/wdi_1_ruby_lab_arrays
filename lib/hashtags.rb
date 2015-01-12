bd3464dc6b22e252473ee279c3065a84a83e5023
David Fisher <tibbon@gmail.com>
2015-01-12 15:52:23 -0500

    exercise ready

---
+++ lib/hashtags.rb
@@ -0,0 +1,2 @@
+def hashtags(tweet)
+end
--- spec/hashtags_spec.rb
+++ spec/hashtags_spec.rb
@@ -1,1 +1,9 @@
 require_relative '../lib/hashtags'
+
+RSpec.describe '#name_formatter' do
+  it 'returns an array of words greater than 3 characters, with a # at the beginning of each word' do
+    original_tweet = 'We ate fifty cheeseburgers for lunch'
+    hashtags = ['#fifty', '#cheeseburgers', '#lunch']
+    expect(hashtags(original_tweet)).to eq hashtags
+  end
+end
