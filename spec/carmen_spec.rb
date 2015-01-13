57bad14d88b52e37c8f7e2ef5b8d22e179ccdb50
Jeffrey Horn <jeffh@generalassemb.ly>
2015-01-13 17:02:20 -0500

    Fix typo

--- spec/carmen_spec.rb
+++ spec/carmen_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/carmen'
 
 RSpec.describe '#carmen' do
-  it 'returns the position of the element '\carmen\' in the array' do
+  it 'returns the position of the element \'carmen\' in the array' do
     expect(carmen(['chris', 'kim', 'carmen'])).to eq 2
     expect(carmen(['linn', 'carmen', 'sandy', 'bob'])).to eq 1
     expect(carmen(['carmen', 'fae', 'mel', 'alice'])).to eq 0
